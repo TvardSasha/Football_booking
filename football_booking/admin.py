e79a8ad39b25352fda80cc8b9707f06d6684bf88
TvardSasha <tvard.san4ik@gmail.com>
2024-02-24 15:16:59 +0200

    Hello

---
+++ football_booking/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from football_booking.models import *
+
+admin.site.register(User)
+admin.site.register(Place)
+admin.site.register(Booking)
---
+++ football_booking/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FootballBookingConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'football_booking'
